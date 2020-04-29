0cbce7cf98bacaea563d825851a379cb87a5562f
David D Lowe <daviddlowe.flimm@gmail.com>
2020-04-29 14:20:43 +0100

    Support Django 3.0 and Python 3.8

--- setup.py
+++ setup.py
@@ -37,6 +37,7 @@ setup(
         'Framework :: Django :: 2.0',
         'Framework :: Django :: 2.1',
         'Framework :: Django :: 2.2',
+        'Framework :: Django :: 3.0',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
@@ -46,6 +47,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'License :: OSI Approved :: MIT License',
     ],
 )
